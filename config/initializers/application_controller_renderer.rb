107fc5a74e1cc8eaad3cba88b5f0af4198ba9abb
shizuka ota <shizukaota@shizukanoMacBook-Pro.local>
2019-02-17 21:24:33 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'ddc94c738cf33f9331498535c397ce183e845afc2a1076feeb75fb4558aa83b7ef1998152722e57fa0e2e9afc003ea1cced13ed6378dee047d899c2fbfb39f55'
